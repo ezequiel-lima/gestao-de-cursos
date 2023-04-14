f8785f327fc48789dd14ccf9a5975cd589ecac07
Ezequiel Lima <joao.lima94@fatec.sp.gov.br>
2023-04-14 16:52:17 -0300

    adicionado tests para viewmodel

---
+++ GestaoCurso.Tests/ViewModels/CategoriaViewModelTests.cs
@@ -0,0 +1,54 @@
+﻿using GestaoCurso.Domain.ViewModels.Categorias;
+
+namespace GestaoCurso.Tests.ViewModels
+{
+    [TestClass]
+    public class CategoriaViewModelTests
+    {       
+        [TestMethod]
+        [DataTestMethod]
+        [DataRow("Programação")]
+        [DataRow("Desenvolvimento")]
+        [DataRow("Arte")]
+        public void Deve_Retornar_Sucesso_Quando_Categoria_For_Valida_Para_Criacao(string nome)
+        {
+            var categoria = new CreateCategoriaViewModel(nome);
+
+            Assert.IsTrue(categoria.IsValid);
+        }
+
+        [TestMethod]
+        [DataTestMethod]
+        [DataRow("")]
+        [DataRow("rRSHccAbeJxPHwEGRUGcQqPtSjMTyBDqxWqgigVFNUKASTRdASrGThMEnhYcaUhdcuiddUWfauUWPSjCEVQnkmzdfYWUdBEtZrEPF")]
+        public void Deve_Retornar_Erro_Quando_Categoria_For_Invalida_Para_Criacao(string nome)
+        {
+            var categoria = new CreateCategoriaViewModel(nome);
+
+            Assert.IsFalse(categoria.IsValid);
+        }
+
+        [TestMethod]
+        [DataTestMethod]
+        [DataRow("Programação")]
+        [DataRow("Desenvolvimento")]
+        [DataRow("Arte")]
+        public void Deve_Retornar_Sucesso_Quando_Categoria_For_Valida_Para_Atualizacao(string nome)
+        {
+            var categoria = new UpdateCategoriaViewModel(nome);
+
+            Assert.IsTrue(categoria.IsValid);
+        }
+
+        [TestMethod]
+        [DataTestMethod]
+        [DataRow("")]
+        [DataRow("rRSHccAbeJxPHwEGRUGcQqPtSjMTyBDqxWqgigVFNUKASTRdASrGThMEnhYcaUhdcuiddUWfauUWPSjCEVQnkmzdfYWUdBEtZrEPF")]
+        public void Deve_Retornar_Erro_Quando_Categoria_For_Invalida_Para_Atualizacao(string nome)
+        {
+            var categoria = new UpdateCategoriaViewModel(nome);
+
+            Assert.IsFalse(categoria.IsValid);
+        }
+    }
+}
---
+++ GestaoCurso.Tests/ViewModels/CursoViewModelTests.cs
@@ -0,0 +1,32 @@
+﻿using GestaoCurso.Domain.ViewModels.Cursos;
+
+namespace GestaoCurso.Tests.ViewModels
+{
+    [TestClass]
+    public class CursoViewModelTests
+    {
+        [TestMethod]
+        [DataTestMethod]
+        [DataRow("Programação", "2035-01-01", "2035-12-31", 30, "A8BB0629-2877-405F-B636-5303B715540A")]
+        public void Deve_Retornar_Sucesso_Quando_Curso_For_Valido_Para_Criacao(string nome, string dataInicio, string dataFim, int quantidadeDeAluno, string categoriaId)
+        {        
+            var curso = new CreateCursoViewModel(nome, DateTime.Parse(dataInicio), DateTime.Parse(dataFim), quantidadeDeAluno, Guid.Parse(categoriaId));
+
+            Assert.IsTrue(curso.IsValid);
+        }
+
+        [TestMethod]
+        [DataTestMethod]
+        [DataRow("", "2035-01-01", "2035-12-31", 30, "A8BB0629-2877-405F-B636-5303B715540A")]
+        [DataRow("rRSHccAbeJxPHwEGRUGcQqPtSjMTyBDqxWqgigVFNUKASTRdASrGThMEnhYcaUhdcuiddUWfauUWPSjCEVQnkmzdfYWUdBEtZrEPF", "2035-01-01", "2035-12-31", 30, "A8BB0629-2877-405F-B636-5303B715540A")]
+        [DataRow("Programação", "2022-01-01", "2035-12-31", 30, "A8BB0629-2877-405F-B636-5303B715540A")]
+        [DataRow("Programação", "2035-02-02", "2035-01-01", 30, "A8BB0629-2877-405F-B636-5303B715540A")]
+        [DataRow("Programação", "2035-01-01", "2035-12-31", -1, "A8BB0629-2877-405F-B636-5303B715540A")]
+        public void Deve_Retornar_Erro_Quando_Curso_For_Invalido_Para_Criacao(string nome, string dataInicio, string dataFim, int quantidadeDeAluno, string categoriaId)
+        {
+            var curso = new CreateCursoViewModel(nome, DateTime.Parse(dataInicio), DateTime.Parse(dataFim), quantidadeDeAluno, Guid.Parse(categoriaId));
+
+            Assert.IsFalse(curso.IsValid);
+        }
+    }
+}
